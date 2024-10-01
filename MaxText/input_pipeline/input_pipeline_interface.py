22c8aa783b070f36eeb838a119d68ed22313505e
FBS <aliu2000@outlook.com>
2024-10-01 19:55:41 +0800

    test

--- MaxText/input_pipeline/input_pipeline_interface.py
+++ MaxText/input_pipeline/input_pipeline_interface.py
@@ -67,6 +67,9 @@ class SyntheticDataIterator:
     output["targets_segmentation"] = jax.numpy.ones(
         (config.global_batch_size_to_load, config.max_target_length), dtype=jax.numpy.int32
     )
+    output["prompt_length"] = jax.numpy.ones(
+        (config.global_batch_size_to_load, config.max_target_length), dtype=jax.numpy.int32
+    )
     return output
 
 
