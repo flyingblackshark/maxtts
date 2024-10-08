2680911188fba0edbf6c9f8a2d3609e4ef702790
FBS <aliu2000@outlook.com>
2024-10-08 10:07:58 +0000

    test

---
+++ setup_decode.sh
@@ -0,0 +1,4 @@
+apt update
+apt install -y libsndfile-dev
+pip3 install git+https://github.com/flyingblackshark/DAC-JAX.git
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
