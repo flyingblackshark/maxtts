2870b9ba9d2a27aa83fbaa9371295185f769bab1
FBS <aliu2000@outlook.com>
2024-10-18 16:27:33 +0000

    test

---
+++ MaxText/layers/fbs_decoder_layer.py
@@ -0,0 +1,124 @@
+"""
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""Transformer model definition."""
+# pylint: disable=arguments-differ
+# pylint: disable=no-name-in-module
+
+from flax import linen as nn
+from jax.sharding import Mesh
+import jax.numpy as jnp
+# from jax.experimental.pallas.ops.tpu import flash_attention
+from layers import attentions
+from layers import embeddings
+from layers import linears
+from layers import normalizations
+from layers import models
+from layers import quantizations
+
+import common_types
+from typing import Optional
+
+Array = common_types.Array
+Config = common_types.Config
+DType = common_types.DType
+Mesh = common_types.Mesh
+ScanIn = common_types.ScanIn
+
+AxisNames = common_types.AxisNames
+BATCH = common_types.BATCH
+KV_BATCH = common_types.KV_BATCH
+LENGTH = common_types.LENGTH
+HEAD = common_types.HEAD
+KV_HEAD = common_types.KV_HEAD
+D_KV = common_types.D_KV
+KV_HEAD_DIM = common_types.KV_HEAD_DIM
+
+
+Embed = embeddings.Embed
+Attention = attentions.Attention
+RMSNorm = normalizations.RMSNorm
+Quant = quantizations.AqtQuantization
+
+
+
+class FBSDecoderLayer(nn.Module):
+  """Transformer decoder layer that attends to the encoder."""
+
+  config: models.Config
+  mesh: Mesh
+  quant: Optional[Quant] = None
+
+  @nn.compact
+  def __call__(
+      self,
+      inputs,
+      decoder_segment_ids,
+      decoder_positions,
+      deterministic,
+      model_mode,
+  ):
+    cfg = self.config
+    mesh = self.mesh
+
+    inputs = nn.with_logical_constraint(inputs, ("activation_batch", "activation_length", "activation_embed"))
+
+    lnx_rms = models.RMSNorm(
+        dtype=cfg.dtype,
+        weight_dtype=cfg.weight_dtype,
+        name="pre_self_attention_layer_norm",
+        kernel_axes=("norm",),
+        epsilon=cfg.normalization_layer_epsilon,
+    )
+    lnx = lnx_rms(inputs)
+
+    lnx = nn.with_logical_constraint(lnx, ("activation_batch", "activation_length", "activation_embed"))
+
+    # MLP block.
+    mlp_lnx = linears.MlpBlock(
+        intermediate_dim=cfg.mlp_dim,
+        activations=cfg.mlp_activations,
+        intermediate_dropout_rate=cfg.dropout_rate,
+        dtype=cfg.dtype,
+        weight_dtype=cfg.weight_dtype,
+        name="mlp",
+        config=cfg,
+        quant=self.quant,
+    )(lnx, deterministic=deterministic)
+    mlp_lnx = nn.with_logical_constraint(mlp_lnx, ("activation_batch", "activation_length", "activation_embed"))
+
+    layer_output = mlp_lnx + lnx
+
+    layer_output = nn.Dropout(rate=cfg.dropout_rate, broadcast_dims=(-2,))(layer_output, deterministic=deterministic)
+
+    layer_output = nn.with_logical_constraint(
+        layer_output,
+        ("activation_batch", "activation_length", "activation_embed"),
+    )
+
+    if cfg.record_internal_nn_metrics:
+      self.sow("intermediates", "activation_mean", jnp.mean(layer_output))
+      self.sow("intermediates", "activation_stdev", jnp.std(layer_output))
+      self.sow(
+          "intermediates",
+          "activation_fraction_zero",
+          jnp.sum(layer_output == 0) / jnp.size(layer_output),
+      )
+
+    if cfg.scan_layers:
+      return layer_output, None
+    else:
+      return layer_output
--- MaxText/max_utils.py
+++ MaxText/max_utils.py
@@ -706,7 +706,7 @@ def create_learning_rate_schedule(config):
 
 # Cross entropy implementation is taken from original T5X codebase:
 # https://github.com/google-research/t5x/blob/ace831eea1e2742b4299cd1a9af7e4f302038351/t5x/losses.py#L25-L101
-@jax.custom_vjp
+#@jax.custom_vjp
 def cross_entropy_with_logits(
     logits: jnp.ndarray, targets: jnp.ndarray, z_loss: float
 ) -> Tuple[jnp.ndarray, jnp.ndarray]:
@@ -738,74 +738,74 @@ def cross_entropy_with_logits(
   return loss, total_z_loss
 
 
-def _cross_entropy_with_logits_fwd(
-    logits: jnp.ndarray, targets: jnp.ndarray, z_loss: float = 0.0
-) -> Tuple[
-    Tuple[jnp.ndarray, jnp.ndarray],
-    Tuple[
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-    ],
-]:
-  """Forward-mode of `cross_entropy_with_logits`."""
-  max_logit = logits.max(axis=-1, keepdims=True)
-  shifted = logits - max_logit
-  exp_shifted = jnp.exp(shifted)
-  sum_exp = jnp.sum(exp_shifted, axis=-1, keepdims=True)
-  log_softmax = shifted - jnp.log(sum_exp)
-  loss = -jnp.sum(targets * log_softmax, axis=-1)
-  # Add auxiliary z-loss term.
-  log_z = jnp.squeeze(jnp.log(sum_exp) + max_logit, axis=-1)
-  total_z_loss = z_loss * jax.lax.square(log_z)
-  loss += total_z_loss
-  return (loss, total_z_loss), (
-      logits,
-      targets,
-      z_loss,
-      exp_shifted,
-      sum_exp,  # pytype: disable=bad-return-type  #jax-ndarray
-      log_softmax,
-      log_z,
-  )
+# def _cross_entropy_with_logits_fwd(
+#     logits: jnp.ndarray, targets: jnp.ndarray, z_loss: float = 0.0
+# ) -> Tuple[
+#     Tuple[jnp.ndarray, jnp.ndarray],
+#     Tuple[
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#     ],
+# ]:
+#   """Forward-mode of `cross_entropy_with_logits`."""
+#   max_logit = logits.max(axis=-1, keepdims=True)
+#   shifted = logits - max_logit
+#   exp_shifted = jnp.exp(shifted)
+#   sum_exp = jnp.sum(exp_shifted, axis=-1, keepdims=True)
+#   log_softmax = shifted - jnp.log(sum_exp)
+#   loss = -jnp.sum(targets * log_softmax, axis=-1)
+#   # Add auxiliary z-loss term.
+#   log_z = jnp.squeeze(jnp.log(sum_exp) + max_logit, axis=-1)
+#   total_z_loss = z_loss * jax.lax.square(log_z)
+#   loss += total_z_loss
+#   return (loss, total_z_loss), (
+#       logits,
+#       targets,
+#       z_loss,
+#       exp_shifted,
+#       sum_exp,  # pytype: disable=bad-return-type  #jax-ndarray
+#       log_softmax,
+#       log_z,
+#   )
 
 
-def _cross_entropy_with_logits_bwd(
-    res: Tuple[
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-        jnp.ndarray,
-    ],
-    g: Tuple[jnp.ndarray, jnp.ndarray],
-) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-  """Backward-mode of `cross_entropy_with_logits`."""
-  g = g[0]  # Ignore z_loss component as that is only used for logging.
-  logits, targets, z_loss, exp_shifted, sum_exp, log_softmax, log_z = res
-  # z-loss term adds the (2 * z_loss * log_z) factor.
-  deriv = (
-      jnp.expand_dims(1 + 2 * z_loss * log_z, -1) * exp_shifted / sum_exp
-      - targets
-  )
-  g_logits = jnp.expand_dims(g, axis=-1) * deriv
-  g_targets = -jnp.expand_dims(g, axis=-1) * log_softmax
-  return (
-      jnp.asarray(g_logits, logits.dtype),
-      jnp.asarray(g_targets, targets.dtype),
-      jnp.array(0.0),
-  )  # sets z-loss coeff gradient to 0
+# def _cross_entropy_with_logits_bwd(
+#     res: Tuple[
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#         jnp.ndarray,
+#     ],
+#     g: Tuple[jnp.ndarray, jnp.ndarray],
+# ) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+#   """Backward-mode of `cross_entropy_with_logits`."""
+#   g = g[0]  # Ignore z_loss component as that is only used for logging.
+#   logits, targets, z_loss, exp_shifted, sum_exp, log_softmax, log_z = res
+#   # z-loss term adds the (2 * z_loss * log_z) factor.
+#   deriv = (
+#       jnp.expand_dims(1 + 2 * z_loss * log_z, -1) * exp_shifted / sum_exp
+#       - targets
+#   )
+#   g_logits = jnp.expand_dims(g, axis=-1) * deriv
+#   g_targets = -jnp.expand_dims(g, axis=-1) * log_softmax
+#   return (
+#       jnp.asarray(g_logits, logits.dtype),
+#       jnp.asarray(g_targets, targets.dtype),
+#       jnp.array(0.0),
+#   )  # sets z-loss coeff gradient to 0
 
 
-cross_entropy_with_logits.defvjp(
-    _cross_entropy_with_logits_fwd, _cross_entropy_with_logits_bwd
-)
+# cross_entropy_with_logits.defvjp(
+#     _cross_entropy_with_logits_fwd, _cross_entropy_with_logits_bwd
+# )
 
 
 def get_abstract_state(model, tx, config, rng, mesh, is_training=True):
